ae926a5b47cf4615fd8762d5633ae640da6ac322
Endode <endertravis9@gmail.com>
2022-11-04 15:49:22 -0400

    Test Commit, fixed the YouTube video stuff on alsogoobers.html. Also organized some files

--- mongoose.ts
+++
@@ -1,8 +0,0 @@
-var redImposterTW: TypeWriter;
-
-function runWhenDoneLoading() {
-    redImposterTW = new TypeWriter("I'm looking for the REAL Chungus, not some red imposter!", 12, false, document.getElementById("redImposterTW"));
-    redImposterTW.type();
-}
-
-document.addEventListener('DOMContentLoaded', runWhenDoneLoading);
